39ef87721bb1af0ce8a5ccd26116f0d4f7ec7964
andritianaa <andritianasteve@gmail.com>
2023-08-20 17:51:55 +0300

    ajouter hover sur image services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit, AfterViewInit {
+  isModal = false
+  showModal(): void {
+    this.isModal = true
+  }
+  hideModal(): void {
+    this.isModal = false
+  }
 
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 500,
+      delay: 50,
+    })
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.refresh()
+    }, 500)
+  }
 }
